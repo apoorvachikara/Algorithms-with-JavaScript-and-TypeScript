5226cb57cab6e9f031f3d60d3847037fe216df52
apoorvachikara <chikara02apoorva@gmail.com>
2022-01-12 14:46:05 +0530

    Updated Algo Comments

--- Javascript/linked list/singly/Interview-questions/palindromeLinkList.js
+++ Javascript/linked list/singly/Interview-questions/palindromeLinkList.js
@@ -1,43 +1,69 @@
+/**
+ * Node - node
+ */
 class Node {
-    constructor (value, next = null) {
-        this.value = value;
-        this.next = next;
-    }
+  /**
+     *
+     * @param {*} value - number or any
+     * @param {*} next - Node
+     */
+  constructor(value, next = null) {
+    this.value = value;
+    this.next = next;
+  }
 }
 
+/**
+ * LinkList - LinkList
+ */
 class LinkList {
-    constructor (value) {
-        this.head = new Node(value);
-    }
+/**
+ *
+ * @param {*} value - number or any
+ */
+  constructor(value) {
+    this.head = new Node(value);
+  }
 
-    insert(value) {
-        this.head = new Node(value, this.head);
-    }
+  /**
+   *
+   * @param {*} value - number or any
+   */
+  insert(value) {
+    this.head = new Node(value, this.head);
+  }
 
-    checkIfPanlindrome() {
-        let current = this.head,
-            simpleStr = '',
-            reverseStr = '';
-        
-        while (current) {
-            simpleStr = simpleStr + current.value;
-            reverseStr = current.value + reverseStr;
-            current = current.next;
-        }
-        console.log(simpleStr, reverseStr);
-        return simpleStr === reverseStr;
-    }
+  // eslint-disable-next-line valid-jsdoc
+  /**
+   * @return - boolean
+   */
+  checkIfPanlindrome() {
+    let current = this.head;
+    let simpleStr = '';
+    let reverseStr = '';
 
-    print() {
-        let current = this.head;
-        const arr = [];
-
-        while (current) {
-            arr.push(current.value);
-            current = current.next;
-        }
-        console.log(arr);
+    while (current) {
+      simpleStr = simpleStr + current.value;
+      reverseStr = current.value + reverseStr;
+      current = current.next;
     }
+    console.log(simpleStr, reverseStr);
+    return simpleStr === reverseStr;
+  }
+
+  /**
+   * Prints the linklist
+   */
+  print() {
+    let current = this.head;
+    const arr = [];
+
+    while (current) {
+      arr.push(current.value);
+      current = current.next;
+    }
+    console.log(arr);
+  }
 }
 
 const l1 = new LinkList(1);
@@ -45,6 +71,6 @@ l1.insert(2);
 l1.insert(2);
 l1.insert(1);
 l1.insert(1);
-  
-console.log(l1.checkIfPanlindrome())
+
+console.log(l1.checkIfPanlindrome());
 l1.print();
