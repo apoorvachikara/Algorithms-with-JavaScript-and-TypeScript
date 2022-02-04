42903344549714b391f698507662500e6a314c0b
apoorvachikara <chikara02apoorva@gmail.com>
2022-02-04 18:40:18 +0530

    ADDING NEW ARRAY QUESTION FIND MISSING

---
+++ Javascript/Arrays/Interview Questions/findMissingElement.js
@@ -0,0 +1,9 @@
+// Given an array of size N-1 such that it only contains
+// distinct integers in the range of 1 to N. Find the missing element.
+
+const checkMissingElement = (array) => {
+    for (const el of array) {
+
+    }
+}
+
