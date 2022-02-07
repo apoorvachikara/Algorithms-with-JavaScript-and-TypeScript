c5c338d4ab6da6962391a9cde762134183c91a06
apoorvachikara <chikara02apoorva@gmail.com>
2022-02-07 20:47:51 +0530

    Adding Array interview Question - Find K largest or smallest elements in the array

---
+++ Javascript/Arrays/Interview Questions/kLargestOrSmallestElements.js
@@ -0,0 +1,15 @@
+// Write an efficient program for printing k largest elements in an array.
+// Elements in an array can be in any order.
+// For example, if the given array is [1, 23, 12, 9, 30, 2, 50] and
+// you are asked for the largest 3 elements i.e., k = 3
+// then your program should print 50, 30, and 23.
+
+
+// O(nlog(n))
+const findKElements = (array, smallest = 0, largest = 0) => {
+  array.sort((a, b) => a - b);
+  return smallest ? array.slice(0, smallest) : array.slice(-largest);
+};
+
+const mockArr = [1, 23, 12, 9, 30, 2, 50];
+console.log(findKElements(mockArr, 0, 3));
