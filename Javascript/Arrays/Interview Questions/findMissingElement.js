d1c001cca3b49486c6fd8445c12f9b2b1c3b9f97
apoorvachikara <chikara02apoorva@gmail.com>
2022-02-07 20:31:19 +0530

    Adding Array interview question - find missing number

--- Javascript/Arrays/Interview Questions/findMissingElement.js
+++ Javascript/Arrays/Interview Questions/findMissingElement.js
@@ -2,8 +2,17 @@
 // distinct integers in the range of 1 to N. Find the missing element.
 
 const checkMissingElement = (array) => {
-    for (const el of array) {
-
+  const missingNumber = [];
+  for (let i = 1; i < array.length; i++) {
+    const currentNum = array[i-1] + 1;
+    if (currentNum !== array[i]) {
+      missingNumber.push(currentNum);
     }
-}
+  }
+  return missingNumber;
+};
+
+const arr = [1, 2, 3, 5, 7, 8, 9];
+console.log(checkMissingElement(arr));
+
 
