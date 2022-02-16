691306cdaca54d9aac0b26cd3dc2a109ba165e5f
apoorvachikara <chikara02apoorva@gmail.com>
2022-02-16 19:08:37 +0530

    Adding selection sort for Typescript

---
+++ Typescript/sorting/selectionSort.ts
@@ -0,0 +1,35 @@
+class SelectionSort {
+
+    /**
+    *
+    * @param {*} arr
+    * @return {*} Array
+    */
+    sort(arr: number[]) {
+        for (let index = 0; index < arr.length -1; index++) {
+            const min = this.min(arr, index);
+            if (arr[index] < arr[min]) {
+              [arr[index], arr[min]] = [arr[min], arr[index]];
+            }
+        }
+    }
+
+    /**
+   *
+   * @param {*} arr
+   * @param {*} start
+   * @return {Array}
+   */
+  min(arr, start = 0) {
+    const len = arr.length;
+    let minIndex = start;
+    for (let i = start+1; i < len; i++) {
+      if (arr[minIndex] > arr[i]) {
+        minIndex = i;
+      }
+    }
+    return minIndex;
+  }
+}
+const selectionSort = new SelectionSort();
+console.log(selectionSort.sort([0, 2, 2, 2, 0, 2, 1, 1]));
