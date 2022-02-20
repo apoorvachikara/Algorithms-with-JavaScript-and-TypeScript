a90d93a680103674512fdcb299def544c99b04b1
apoorvachikara <chikara02apoorva@gmail.com>
2022-02-20 13:11:22 +0530

    Adding new finding Unique element problem to array

---
+++ Javascript/Arrays/Interview Questions/findingUniqueElement.js
@@ -0,0 +1,37 @@
+/**
+ * Find the unique first occurenece element in the array
+ * const arr = [2, 3, 4, 4, 3, 2, 5, 7, 7, 6, 7];
+ * Here, there are two unique elements [5, 6], but
+ * 5 occurence is at the begining so `5` will be answer
+ *
+ * Similar Question is is string patter, find the first unique
+ * character in string
+ * const str = "Hello, Hi is the first thing."
+ * Answer will be `e`
+ */
+
+/**
+ *
+ * @param {number} array
+ * @return {number}
+ */
+const findFirstUniqueOccurence = (array) => {
+  const elementMapping = new Map();
+  let unique= '';
+
+  for (let index = 0; index < array.length; index++) {
+    if (!elementMapping.has(array[index])) elementMapping.set(array[index], 1);
+    else elementMapping.set(array[index], elementMapping.get(array[index]) + 1);
+  }
+
+  for (const [key, value] of elementMapping) {
+    if (value === 1) {
+      unique = key;
+      break;
+    }
+  }
+
+  return unique ? unique : -1;
+};
+const arr = [2, 3, 4, 4, 3, 2, 5, 7, 7, 6, 7];
+console.log(findFirstUniqueOccurence(arr));
