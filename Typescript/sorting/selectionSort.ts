9aa50918df91e8200a38d35567f0befe686db65d
apoorvachikara <chikara02apoorva@gmail.com>
2022-02-23 14:40:54 +0530

    Changes to selectionSort in Typescript

--- Typescript/sorting/selectionSort.ts
+++ Typescript/sorting/selectionSort.ts
@@ -8,14 +8,18 @@ class SelectionSort {
     sort(arr: number[]) {
         for (let index = 0; index < arr.length -1; index++) {
             const min = this.min(arr, index);
-            if (arr[index] < arr[min]) {
-              [arr[index], arr[min]] = [arr[min], arr[index]];
+            if (arr[index] > arr[min]) {
+              const temp = arr[min];
+              arr[min] = arr[index];
+              arr[index] = temp;
+              // add more memory so removing it
+              // [arr[index], arr[min]] = [arr[min], arr[index]];
             }
         }
         return arr;
     }
 
-    /**
+  /**
    *
    * @param {*} arr
    * @param {*} start
