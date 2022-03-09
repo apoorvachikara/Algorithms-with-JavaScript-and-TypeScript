fa70637452e145b1a9a2106b69349ef71f417f7a
apoorvachikara <chikara02apoorva@gmail.com>
2022-03-09 14:06:48 +0530

    To check if the string is palindrome - Javascript

---
+++ Javascript/Strings/checkIfStringIsPalindrome.js
@@ -0,0 +1,19 @@
+/**
+ * Checking string is palindrome with built-in function 
+ * @param {*} string 
+ * @return {boolean}
+ */
+const isPalindrome = (string) => {
+  return string === string.split('').reverse().join('');
+}
+
+const isPalindrome_Loops = (string) => {
+    let reverse = '';
+    for (let len = string.length, i = len - 1; i > -1; i--) {
+        reverse += string[i];
+    }
+    return reverse === string;
+}
+
+const str = "NITINI";
+console.log(isPalindrome(str), isPalindrome_Loops(str));
