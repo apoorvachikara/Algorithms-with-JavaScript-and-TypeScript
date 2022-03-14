1447ae72cfd92efde5c925adf2225888ba5f6122
apoorvachikara <chikara02apoorva@gmail.com>
2022-03-14 22:03:30 +0530

    Added code to RomanToInteger in Javascript

--- Javascript/Strings/romanToInteger.js
+++ Javascript/Strings/romanToInteger.js
@@ -2,7 +2,56 @@
  * 
  * @param {*} string 
  * @returns {string}
+ * There are 7 symbols in Roman numeral system 
+ *  I - 1,
+ *  V - 5,
+ *  X - 10,
+ *  L - 50,
+ *  C - 100,
+ *  D - 500,
+ *  M - 1000
+ *  
+ * Whenever we have any roman number we read it from left to right
+ * and whenever we found smaller number first and then large number 
+ * we subtract it else we add it 
+ *  for example - XVIV - X and V are ascending and then we have I 
+ *  and again I that makes it 10 + 5 - 1 + 5 = 19
  */
 const romanToInteger = (string) => {
-    return '';
+    const romanTable = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    }
+    let result = 0;
+    // for (let i =0, len = string.length; i < len; i++) {
+    //     const current = romanTable[string[i]];
+    //     const next = romanTable[string[i+1]];
+    //     if ( current > next) {
+    //         result += current;
+    //     } else {
+    //         result += next - current;
+    //         i++
+    //     }
+    // }
+
+    for (let i=0; i < string.length; i++){
+        const cur = romanTable[string[i]];
+        const next = romanTable[string[i+1]];
+
+        if (cur < next){
+            result += next - cur // IV -> 5 - 1 = 4
+            i++
+        } else {
+            result += cur
+        }
+    }
+
+    return result; 
 }
+
+console.log(romanToInteger('XXVIV'));
