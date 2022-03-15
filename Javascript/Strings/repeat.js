2f0d3b87e4a956b135c346aaf0c1ce428c1705f2
apoorvachikara <chikara02apoorva@gmail.com>
2022-03-15 20:57:06 +0530

    Added execption for out of bound values - repeat.js - Javascript

--- Javascript/Strings/repeat.js
+++ Javascript/Strings/repeat.js
@@ -1,8 +1,13 @@
 const repeat = (count, string) => {
+    if (count < 0) throw('Out of bound value');
     if (count === 0) return '';
     if (count === 1) return string;
 
     return string + repeat(count - 1, string);
 }
 
-console.log(repeat(5, 'abc'));
+try {
+    console.log(repeat(-1, 'abc'));
+} catch (error) {
+    console.error(error);
+}
