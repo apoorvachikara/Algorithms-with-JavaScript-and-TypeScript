ed34361abce36ecc333bddb6b9840bcda55d5078
apoorvachikara <chikara02apoorva@gmail.com>
2022-03-28 10:46:55 +0530

    Added compareTwoArrays.js - Javascript Arrays

--- Javascript/Arrays/Interview Questions/compareTwoArrays.js
+++ Javascript/Arrays/Interview Questions/compareTwoArrays.js
@@ -1,3 +1,31 @@
 /**
  * 
+ * @param {*} array1 
+ * @param {*} array2 
+ * @return {boolean}
+ * Time - O(n + nlog(n)) Space - O(n)
  */
+const compare = (array1, array2) => {
+    // length of arrays
+    const l1 = array1.length;
+    const l2 = array2.length; 
+    if (l1 !== l2) return false;
+
+    // sort and match if they are same
+    array1.sort((a,b) => a - b);
+    array2.sort((a,b) => a - b);
+
+    for (let index = 0; index < array1.length; index++) {
+        if (array1[index] !== array2[index]) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+
+const array1 = [0, -1, -2, 1, 4];
+const array2 = [1, 4, -1, -2, 0];
+
+console.log(compare(array1, array2));
