9d5950ab230911880abe79e7e3e9b20cb3f9dfb2
apoorvachikara <chikara02apoorva@gmail.com>
2022-03-28 12:27:36 +0530

    Updated compareTwoArrays.js - Javascript Arrays

--- Javascript/Arrays/Interview Questions/compareTwoArrays.js
+++ Javascript/Arrays/Interview Questions/compareTwoArrays.js
@@ -24,8 +24,34 @@ const compare = (array1, array2) => {
     return true;
 }
 
+const compare_v1 = (array1, array2) => {
+    const l1 = array1.length;
+    const l2 = array2.length; 
+
+    if (l1 !== l2) return false;
+
+    const map = new Map();
+
+    for (let index = 0; index < array1.length; index++) {
+       map.get(array1[index]) ? map.set(array1[index], map.get(array1[index]) + 1) :
+                                map.set(array1[index], 1);
+        
+    }
+
+    for (let index = 0; index < array2.length; index++) {
+        const count = map.get(array2[index]);
+       if (count > -1) {
+            map.set(array2[index], count - 1);
+            continue;
+       }
+       return false;
+    }
+
+    return true;
+}
 
 const array1 = [0, -1, -2, 1, 4];
-const array2 = [1, 4, -1, -2, 0];
+const array2 = [1, 4, -1, -2, 5];
 
 console.log(compare(array1, array2));
+console.log(compare_v1(array1, array2));
