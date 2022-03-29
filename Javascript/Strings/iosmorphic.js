3b7076154a2f40e7b16253f9ca992220b384329c
apoorvachikara <chikara02apoorva@gmail.com>
2022-03-29 15:35:19 +0530

    Added isIsomorphic.js - Javascript Strings

---
+++ Javascript/Strings/iosmorphic.js
@@ -0,0 +1,15 @@
+/**
+ * 
+ * @param {string} string1 
+ * @param {string} string2 
+ * @returns {boolean} 
+ * 
+ * str1 = "abab" str2 = "xyxy"
+ * A string is isomopphic if every character in string 1 mapped to str2 and viceversa;
+ */
+const isIsomorphic = (string1, string2) => {
+    if (string1.length !== string2) return false;
+    const count = new Map();
+
+    return true;    
+}
