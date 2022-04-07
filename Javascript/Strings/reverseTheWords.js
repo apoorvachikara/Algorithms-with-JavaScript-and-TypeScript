098b97ec924df8b130d05936fca4407705d8490d
apoorvachikara <chikara02apoorva@gmail.com>
2022-04-07 13:31:24 +0530

    Added reverseTheWords.js - Javascript Strings

---
+++ Javascript/Strings/reverseTheWords.js
@@ -0,0 +1,41 @@
+/**
+ * Given a string you need to revers only the string not words
+ * ex : "I am Apoorva Chikara"
+ * o/p: "Chikara Apoorva am I";
+ * 
+ * It should maintain any additional whitespaces in string
+ * ex : "I am Apoorva Chikara  "
+ * o/p: "  Chikara Apoorva am I";
+ * @param {*} string 
+ * @return {string}
+ */
+const reverse = (string) => {
+    const words = [];
+    let start = 0;
+
+    for (let index = 0, len = string.length; index < len; index++) {
+        if (string[index] === ' ') {
+            words.push(string.slice(start, index));
+            start = index +1 ;
+        }
+    }
+    words.push(string.slice(start))
+    console.log(words)
+    let s = 0;
+    let e= words.length -1;
+
+    while(s < e) {
+        const temp = words[s];
+        words[s] = words[e];
+        words[e] = temp;
+        s += 1;
+        e -= 1; 
+    }
+    return words.join(' ');
+}
+
+
+
+console.log(reverse('I  am  Apoorva Chikara') === 'Chikara Apoorva  am  I');
+console.log(reverse('&&  Apoorva Chikara  &&  ') === '  &&  Chikara Apoorva  &&');
+console.log(reverse('Text     ') === '     Text');
