bba0545d05e75bc1c289446e14926a0ab4430a93
apoorvachikara <chikara02apoorva@gmail.com>
2022-04-19 15:38:15 +0530

    Updated invertTree.js - Javascript Trees

--- Javascript/Trees/invertTrees.js
+++ Javascript/Trees/invertTrees.js
@@ -30,6 +30,11 @@ class BST {
     }
 
 
+    /**
+     * Using recursion
+     * @param {*} tree 
+     * @return {empty}
+     */
     invert(tree = null) {
         if (tree === null) return;
         this.swap(tree);
@@ -37,6 +42,21 @@ class BST {
         this.invert(tree.right);
     }
 
+    /**
+     * Using queue 
+     * @param {*} tree
+     * @return {empty} 
+     */
+    invertUsingQueue(tree = null) {
+        const queue = [tree];
+        while(queue.length) {
+            const current = queue.shift();
+            if (current === null) continue;
+            this.swap(current);
+            queue.push(current.left);
+            queue.push(current.right);
+        }
+    }
     swap(tree) {
         const temp = tree.left;
         tree.left = tree.right;
@@ -51,6 +71,10 @@ tree.insert(5).insert(2).insert(5).insert(1).insert(15).insert(13).insert(22).in
 
 console.dir(JSON.parse(JSON.stringify(tree)), {depth: null})
 
-tree.invert(tree);
+tree.invert(tree); // invert the tree
+tree.invert(tree); // invert it again
 
-console.dir(tree, {depth: null});
+console.dir(JSON.parse(JSON.stringify(tree)), {depth: null});
+tree.invertUsingQueue(tree);
+
+console.dir(JSON.parse(JSON.stringify(tree)), {depth: null});
