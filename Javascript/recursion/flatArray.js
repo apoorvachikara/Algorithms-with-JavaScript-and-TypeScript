11c5e7b9a41715a3eb775b44ce7130ec22b8d625
apoorvachikara <chikara02apoorva@gmail.com>
2022-04-25 13:54:46 +0530

    Updated flatArray.js with O/p - Javascript Recursion

--- Javascript/recursion/flatArray.js
+++ Javascript/recursion/flatArray.js
@@ -27,5 +27,5 @@ const flatArray = (input) => {
 
 const array = [[[1], [2], [3]], [[4, [5], [6, [7, [8]]]]]];
 
-console.log(flatArray(array));
+console.log(flatArray(array)); //[ 1, 2, 3, 4, 5, 6, 7, 8 ]
 
