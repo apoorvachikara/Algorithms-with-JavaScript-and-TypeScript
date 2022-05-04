955fff14ece318fd47f7e463db704016c149d63d
Apoorva Chikara <chikara02apoorva@gmail.com>
2022-05-04 17:47:22 +0530

    Updated generateGCD.js - Javascript Math

--- Javascript/Math/generateGCD.js
+++ Javascript/Math/generateGCD.js
@@ -1,3 +1,9 @@
+/**
+ * 
+ * @param {number} a 
+ * @param {number} b 
+ * @return {number} 
+ */
 const GCD = (a, b) => {
     if ( a === 0 && b === 0) return 0;
     else if (a === 0) return b;
@@ -8,3 +14,16 @@ const GCD = (a, b) => {
 }
 
 console.log(GCD(98, 56));
+
+/**
+ * 
+ * @param {*} a 
+ * @param {*} b 
+ * @return {number} 
+ */
+const GCD_EUCLIDEAN = (a, b) => {
+    if (b === 0) return a;
+    return GCD_EUCLIDEAN(b, a % b);
+}
+
+console.log(GCD_EUCLIDEAN(98, 56));
