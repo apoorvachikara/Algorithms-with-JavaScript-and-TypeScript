bd351bc6113d78b3249ef1df68e1b25ec167ae6a
Apoorva Chikara <chikara02apoorva@gmail.com>
2022-05-13 17:37:04 +0530

    Added processingQueues.js - Javascript Random

---
+++ Javascript/Random/processingQueues.js
@@ -0,0 +1,46 @@
+class Processor {
+    constructor(opt) {
+        Object.assign(this.opts, opt);
+    }
+    requestsQueue = [];
+    queue = [];
+    opts = {
+        len: 0
+    }
+    execute(request) {
+       if (this.requestsQueue.length <= this.opts.len) {
+           this.requestsQueue.push(request);
+           return this.requestProcess();
+       } else {
+           // add request to queue.
+           this.queue.shift(request);
+       }
+    }
+   
+    async requestProcess() {
+        while(this.requestsQueue.length) {
+            debugger
+            const request = this.requestsQueue.pop();
+            const response = await request();
+            return response;
+        }
+        this.fetchMoreRequestFromQueue();
+    }
+   
+    async fetchMoreRequestFromQueue() {
+       let count = 0;
+       while(this.opts.len < count) {
+           this.requestsQueue.push(this.queue.pop());
+           count++;
+       }
+    }
+   }
+
+const requests = () => {
+    return new Promise((res, rej) => {
+        setTimeout(() => {
+            res(`Request ${Math.random().toFixed()}`)
+        }, Math.random() * 1000)
+    })
+}
+
